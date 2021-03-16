29a06c11073f00ed7c74e0b9e5213927610f1014
WCL - Martin Warnett <enquiries@warnettconsultingltd.co.uk>
2021-03-16 14:54:04 +0000

    Example showing basic mathematic operations

---
+++ basic_languages_structures/basic_numbers.py
@@ -0,0 +1,39 @@
+print("NUMERIC ADDITION OPERATIONS")
+firstNumber = 8
+secondNumber = 53
+print("Basic addition", firstNumber, "+", secondNumber, "=", firstNumber+secondNumber)
+print()
+
+print("NUMERIC SUBTRACTION OPERATIONS")
+firstNumber = 34
+secondNumber = 12
+print("Basic subtraction", firstNumber, "-", secondNumber, "=", firstNumber-secondNumber)
+firstNumber = 13
+secondNumber = 15
+print("Basic subtraction with negative result", firstNumber, "-", secondNumber, "=", firstNumber-secondNumber)
+print()
+
+print("NUMERIC MULTIPLICATION OPERATIONS")
+firstNumber = 7
+secondNumber = 8
+print("Basic multiplication result", firstNumber, "*", secondNumber, "=", firstNumber*secondNumber)
+print()
+
+print("NUMERIC DIVISION OPERATIONS")
+firstNumber = 84
+secondNumber = 5
+print("Basic division result", firstNumber, "/", secondNumber, "=", firstNumber/secondNumber)
+firstNumber = 67
+secondNumber = 12
+print("Decimal division result", firstNumber, "/", secondNumber, "=", firstNumber//secondNumber)
+firstNumber = 41
+secondNumber = 4
+print("Remainder from division", firstNumber, "%", secondNumber, "=", firstNumber%secondNumber)
+print()
+
+print("EXPONENTS")
+print("2 to the power of 0 is ", 2**0)
+print("2 to the power of 1 is ", 2**1)
+print("2 to the power of 2 is ", 2**2)
+print("2 to the power of 3 is ", 2**3)
+
